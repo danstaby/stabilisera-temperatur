7b0d5477c7a84d59a78a856fdaf5cbeb31184f58
Dan Ståby <dstaby@gmail.com>
2012-03-15 16:52:04 +0100

    Added Newton-Raphson algorithm to fem script. Todo: Add a picard iteration as an initial guess for Newton-Raphson

--- code/pdesolver/femconvection.m
+++ code/pdesolver/femconvection.m
@@ -18,16 +18,16 @@ h = 1;
 
 g = 9.81;
 beta = 3.67e-3; %### NOTE #### T-dependent. Fix later
-alpha = 1.9e-5; %Pressure and water dependant
+alpha =1.9e-5; %Pressure and water dependant
 rho = 1.2920; %Pressure and water dependant
 nu = 13; %Temperature and pressure dependant
 penalty = 1e7; %Which penalty?
+Tref = -10;
 
+TneumannConditions = [0, NaN, 0, NaN];
+TneumannTConditions = [NaN, NaN, NaN, NaN];
 
-TneumannConditions = [10*h 0, NaN, 0];
-TneumannTConditions = [-h, NaN, NaN, NaN];
-
-TdirichletConditions = [NaN, NaN, 0, NaN];
+TdirichletConditions = [NaN, 20, NaN, 0];
 
 UdirichletConditions = [NaN, NaN, NaN, 0];
 UneumannConditions = [NaN, NaN, NaN, NaN];
@@ -70,6 +70,7 @@ GT = sparse(pCount, 1);
 Gu = sparse(pCount, 1);
 Gw = sparse(pCount, 1);
 QT = sparse(pCount, pCount);
+LM = sparse(pCount, pCount);
 
 divxx = sparse(pCount, pCount);
 divxz = sparse(pCount, pCount);
@@ -93,6 +94,7 @@ Uneunan = isnan(UneumannConditions(e(5,:)));
 UneuTnan = isnan(UneumannTConditions(e(5,:)));
 Udirnan = isnan(UdirichletConditions(e(5,:)));
 
+
 Wneunan = isnan(WneumannConditions(e(5,:)));
 WneuTnan = isnan(WneumannTConditions(e(5,:)));
 Wdirnan = isnan(WdirichletConditions(e(5,:)));
@@ -105,9 +107,9 @@ Wneumann = e([1 2 5], find(~Wneunan));
 WneumannT = e([1 2 5], find(~WneuTnan));
 Wdirichlet = e([1 2 5], find(~Wdirnan));
 
-Uneumann = e([1 2 5], find(~Wneunan));
-UneumannT = e([1 2 5], find(~WneuTnan));
-Udirichlet = e([1 2 5], find(~Wdirnan));
+Uneumann = e([1 2 5], find(~Uneunan));
+UneumannT = e([1 2 5], find(~UneuTnan));
+Udirichlet = e([1 2 5], find(~Udirnan));
 
 
 %Create derivative matrix
@@ -137,21 +139,27 @@ fprintf(1, 'Assembling stiffness tensors...');
 Tx = sptensor(3,3,3);
 Tz = sptensor(3,3,3);
 
+
+
+voltemp = [2, 1, 1; 1,2,1;1,1,2]/12;
+
 for k = 1:tCount
-  corn = t(1:3,k)';
+  %corn = t(1:3,k)';
   
-  vols = sptensor(area(k,1)*(ones(3,3)/12 + eye(3,3)/12));
+  
+  vols = full(area(k,1))*voltemp;
   %This formula has been taken from
   %http://users.wpi.edu/~sullivan/WebSite-ME515/Lectures/FiniteElement/Triangles/Triangles.htm
   
   
   for j = 1:3
-    Tx(:,:,j) = vols*basedx(corn(j), k);
-    Tz(:,:,j) = vols*basedz(corn(j), k);
+    Tx(:,:,j) = sptensor(vols*basedx(t(j,k)', k));
+    Tz(:,:,j) = sptensor(vols*basedz(t(j,k)', k));
   end
+  
 
-  TensX(corn, corn, corn) = TensX(corn, corn, corn) + Tx;
-  TensZ(corn, corn, corn) = TensZ(corn, corn, corn) + Tz;
+  TensX(t(1:3,k)', t(1:3,k)', t(1:3,k)') = TensX(t(1:3,k)', t(1:3,k)', t(1:3,k)') + Tx;
+  TensZ(t(1:3,k)', t(1:3,k)',t(1:3,k)') = TensZ(t(1:3,k)', t(1:3,k)', t(1:3,k)') + Tz;
   
 end
 fprintf(1, ' done!\n');
@@ -189,13 +197,22 @@ fprintf(1, 'Assembling load vector...');
 
 for k = 1:tCount
   
-  fw(t(1:3,k),1) = fw(t(1:3,k),1) + area(k)/3;
+  fw(t(1:3,k),1) = fw(t(1:3,k),1) + -Tref*area(k)/3;
 end
 
 fw = g*beta*fw;
 
 fprintf(1, ' done!\n');
 
+%Assembling load matrix
+
+fprintf(1, 'Assembling inhomogenous stiffness matrix...');
+
+for j=1:tCount
+  LM(t(1:3,j), t(1:3,j)) = LM(t(1:3,j), t(1:3,j)) + area(j,1)*voltemp;
+end
+
+fprintf(1, ' done!\n');
 
 %Enforce neumann conditions
 fprintf(1, 'Enforcing neumann conditions...')
@@ -247,6 +264,8 @@ uu(Udirichlet(2,:)) = UdirichletConditions(Udirichlet(3,:));
 wu(Wdirichlet(1,:)) = WdirichletConditions(Wdirichlet(3,:));
 wu(Wdirichlet(2,:)) = WdirichletConditions(Wdirichlet(3,:));
 
+
+
 [int, ca, cb] = intersect(Tdirichlet(1,:), Tdirichlet(2,:));
 
 ind = find(Tdirichlet(3,ca) ~= Tdirichlet(3,cb));
@@ -273,38 +292,152 @@ Wu(int(ind)) = 0.5*(WdirichletConditions(Wdirichlet(3,ca(ind))) + ...
 
 %Update load vectors with dirichlet conditions
 
+
 fT = squeeze(sptensor(fT));
 fu = squeeze(sptensor(fu));
 fw = squeeze(sptensor(fw));
 
 
-fT = fT - squeeze(sptensor(alpha*(A+QT)*Tu)) - ttv(ttv(TensX, Tu,3),uu,2) - ttv(ttv(TensZ, Tu, 3),wu,2);
+
+fT = fT - squeeze(sptensor(alpha*(A-QT)*Tu)) - ttv(ttv(TensX, Tu,3),uu,2) - ttv(ttv(TensZ, Tu, 3),wu,2);
 fu = fu - ttv(ttv(TensX, uu,3),uu,2) - ttv(ttv(TensZ, uu, 3),wu,2) - ...
-     squeeze(sptensor(nu*A*uu - divxx*uu - divxz*wu));
+     squeeze(sptensor(nu*A*uu + penalty*(divxx*uu + divxz*wu)/rho));
 fw = fw - ttv(ttv(TensX, wu,3),uu,2) - ttv(ttv(TensZ, wu, 3),wu,2) - ...
-     squeeze(sptensor(nu*A*wu - divzx*uu- divzz*wu));
+     squeeze(sptensor(nu*A*wu -alpha*beta*LM*Tu + penalty*(divzx*uu + divzz*wu)/rho));
 
 %b = b - (A+Q) * u;
 fprintf(1, ' done!\n');
 
 
 %Solve system
-fprintf(1, 'Solving system...');
-%FreeNodes=setdiff(1:pCount,unique(dirichlet));
+fprintf(1, 'Preparing for solution...');
 
+ITERMAX = 10;
+epsilon = 1e-5;
+
+TFreeNodes = setdiff(1:pCount,unique(Tdirichlet));
+uFreeNodes = setdiff(1:pCount, unique(Udirichlet));
+wFreeNodes = setdiff(1:pCount, unique(Wdirichlet));
+
+TFreeCount = size(TFreeNodes,2);
+uFreeCount = size(uFreeNodes,2);
+wFreeCount = size(wFreeNodes,2);
+
+uStart = 1;
+uEnd = uFreeCount;
+wStart = uEnd + 1;
+wEnd = wStart + wFreeCount - 1;
+TStart = wEnd + 1;
+TEnd = TStart + TFreeCount - 1;
+
+resvec = zeros(uFreeCount+wFreeCount + TFreeCount, 1);
+x = zeros(uFreeCount+wFreeCount+TFreeCount,1);
+fprintf(1, ' done!\n');
+
+fprintf(1, '\nSolving system with Newton-Raphson:\n');
+disp(['Max iterations: ' num2str(ITERMAX) ', Epsilon: ' num2str(epsilon)])
+fprintf(1, '\n')
+res = epsilon + 1;
+iterCount = 0;
+
+
+
+%Make initial guess
+uGuess = 1*ones(uFreeCount,1);
+TGuess = -10*ones(TFreeCount,1);
+wGuess = 1*ones(wFreeCount,1);
+
+while(res > epsilon && iterCount < ITERMAX)
+  
+  %Update function value
+  
+  resvec(uStart:uEnd,1) = double(ttv(ttv(TensX(uFreeNodes, uFreeNodes, uFreeNodes), uGuess,3),uGuess,2) + ...
+      ttv(ttv(TensZ(uFreeNodes, wFreeNodes, uFreeNodes), uGuess, 3),wGuess,2)) +  ...
+      nu*A(uFreeNodes, uFreeNodes)*uGuess + penalty*(divxx(uFreeNodes, uFreeNodes)*uGuess + ...
+      divxz(uFreeNodes, wFreeNodes)*wGuess)/rho - ...
+      double(fu(uFreeNodes));
+
+  
+  resvec(wStart:wEnd,1) = double(ttv(ttv(TensX(wFreeNodes, uFreeNodes,wFreeNodes), wGuess,3),uGuess,2) + ...
+      ttv(ttv(TensZ(wFreeNodes, wFreeNodes, wFreeNodes), wGuess,3),wGuess,2)) + ... 
+      nu*A(wFreeNodes, wFreeNodes)*wGuess + ...
+      penalty*(divzx(wFreeNodes,uFreeNodes)*uGuess + ...
+	       divzz(wFreeNodes,wFreeNodes)*wGuess)/rho - alpha*beta*LM(wFreeNodes,TFreeNodes)*TGuess - double(fw(wFreeNodes));
+
+
+
+						
+  resvec(TStart:TEnd,1) = double(ttv(ttv(TensX(TFreeNodes, uFreeNodes,TFreeNodes),TGuess,3),uGuess,2)+...
+      ttv(ttv(TensZ(TFreeNodes, wFreeNodes, TFreeNodes), TGuess,3), wGuess,2)) + ...
+      alpha*((A(TFreeNodes, TFreeNodes)-QT(TFreeNodes,TFreeNodes))*TGuess) ...
+	     -double(fT(TFreeNodes));
+
+  %Calculate Jacobian
+
+  Jacobian = sparse(uFreeCount + wFreeCount + TFreeCount,...
+		    uFreeCount+wFreeCount + TFreeCount);
+  
+  
+  %Jacobian(uStart:uEnd, uStart:uEnd) = double(ttv(TensX, uGuess),2)
+  Jacobian(uStart:uEnd,uStart:uEnd) = double(ttv(TensZ(uFreeNodes, wFreeNodes,uFreeNodes),wGuess,2) ...
+				    + 2*ttv(TensX(uFreeNodes,uFreeNodes, uFreeNodes), uGuess, 2)) ...
+                                    + penalty*divxx(uFreeNodes,uFreeNodes)/rho...
+                                    + nu*A(uFreeNodes,uFreeNodes);
+
+  Jacobian(wStart:wEnd,wStart:wEnd) = double(ttv(TensX(wFreeNodes, uFreeNodes, wFreeNodes),uGuess,2) ...
+				    + 2*ttv(TensZ(wFreeNodes,wFreeNodes,wFreeNodes),wGuess,2)) ...
+                                    + penalty*divzz(wFreeNodes,wFreeNodes)/rho ...
+                                    + nu*A(wFreeNodes, wFreeNodes);
+  
+  Jacobian(TStart:TEnd,TStart:TEnd) = double(ttv(TensX(TFreeNodes,uFreeNodes,TFreeNodes),uGuess,2)...
+				    + ttv(TensZ(TFreeNodes,wFreeNodes,TFreeNodes),wGuess,2))...
+                                    + alpha*(A(TFreeNodes,TFreeNodes)-QT(TFreeNodes, TFreeNodes));
+
+  
+  %Note the factor 2 in the above expressions. That factor deals
+  %with the square terms in the LHS of the equation
+
+  %Solve for dx
+    
+  %[U S V] = svds(Jacobian);
+  %dx =  U*pinv(S)*V'*(-resvec); %Jacobian\(-resvec);
+
+  dx = Jacobian\(-resvec);
+
+  %Update vectors
+  x = x + dx;
+  uGuess = x(uStart:uEnd,1);
+  wGuess = x(wStart:wEnd,1);
+  TGuess = x(TStart:TEnd,1);
+  res=norm(resvec);
+  iterCount = iterCount + 1;
+  disp(['Iteration ' num2str(iterCount) ' done with residual ' num2str(res)])
+end
+
+if(res < epsilon)
+  fprintf(1, '\nFound convergence\n\n');
+else
+  fprintf(1, '\nNewton Raphson failed!\n\n');
+end
+
+Tu(TFreeNodes) = TGuess;
+uu(uFreeNodes) = uGuess;
+Wu(wFreeNodes) = wGuess;
 %u(FreeNodes) = (A(FreeNodes, FreeNodes)+Q(FreeNodes, FreeNodes))\ ...
 %    (b(FreeNodes) +G(FreeNodes));
-fprintf(1, ' done!\n\n')
+
 %Display
 time = toc;
 
 disp(['Execution time: ' num2str(time) ' s']);
 disp(['Triangle count: ' num2str(tCount)])
-%disp(['Degrees of freedom: ' num2str(max(size(FreeNodes)))])
-%figure(1)
-%pdeplot(p,e,t,'xydata',u,'mesh',showgrid);
+disp(['Degrees of freedom: ' num2str(TFreeCount + wFreeCount + uFreeCount)])
 
-%figure(2)
+figure(3)
 pdemesh(p,e,t)
 
+figure(2)
+
+quiver(p(1,:)', p(2,:)', uu, wu);
 figure(1)
+pdeplot(p,e,t,'xydata',Tu,'mesh',showgrid);
