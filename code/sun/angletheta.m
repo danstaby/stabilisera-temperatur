971d9b12018988bca21246b7f48a31fa3a7edf7e
Mats Lindstrom <mats.at.lindstrom@gmail.com>
2012-04-01 16:33:07 +0200

    Modified and added code for sun calculation

--- code/sun/angletheta.m
+++ code/sun/angletheta.m
@@ -1,7 +1,10 @@
 function theta = angletheta(alpha, beta, gamma)
 
-% gamma = vinkel mellan norr och (fönstrets normal - 90)
-beta = beta - 90 - gamma;
+% gamma = vinkel mellan norr och fönstrets normal,
+% öster om norr positivt
+
+beta = beta - gamma;
+
 if beta<-90 || beta>90 || alpha <0
     theta = 90;
 else
