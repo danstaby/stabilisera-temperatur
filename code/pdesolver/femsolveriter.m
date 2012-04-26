b5ea9a03599f0c2a8f9c5e40ce652c9b19632a3b
Dan Ståby <dstaby@gmail.com>
2012-04-26 08:18:24 +0200

    Added more code and an improved version of newton raphson

---
+++ code/pdesolver/femsolveriter.m
@@ -0,0 +1,313 @@
+function ret = femsolveriter(fileName, Tout, VarName, Range)
+%femsolveriter(fileName, Tout, VarName, Range)
+%
+%This function solves the navier stokes equations for
+%a specified problem area.
+%fileName - path to file specifying problem area
+%Tout - The temperature outside
+%VarName - String that contains the name of the variable to varied
+%Range - Range of the variable. [Start, Step, End]
+
+global TensX TensZ divxx divzz divzx divxz A QT LM alpha beta nu rho penalty Gu Gw GT
+
+
+h = 50;
+
+g = 9.81;
+beta = 3.67e-3; %### NOTE #### T-dependent. Fix later
+alpha =1.9e-5; %Pressure and water dependant
+rho = 1.2920; %Pressure and water dependant
+nu = 1.3e-5;%1.3e-5;%1.3e-5; %Temperature and pressure dependant
+penalty = 1e7; %Which penalty?
+sigma = 5.670373e-8;
+kelvin = 273.15;
+Tref = Tout+kelvin;
+T0 = Tref+3; %A guess for wall temperature
+TL = -20+kelvin;
+Tin = 20 + kelvin;
+Uvalue = 1.1858; %U-Value of the south, east and west walls
+kAir = 0.0243;
+
+ITERMAX = 400; %Max iterations for Newton Raphsons method
+epsilon = 1e-5; %Break point for NR-convergence 
+
+
+WallID = 5; %Identifier of the edge that reresents the wall.
+
+meantemp = 0;
+temperr = 0.05;
+newtemp = T0;
+
+fprintf(1,'Loading problem ...');
+ S = load(fileName); %Loading problem specific variables
+ p = S.p;
+ e = S.e;
+ t = S.t;
+ tCount = S.tCount;
+ pCount = S.pCount;
+ eCount = S.eCount;
+ A = sparse(S.A);
+ divxx = sparse(S.divxx);
+ divxz = sparse(S.divxz);
+ divzx = sparse(S.divzx);
+ divzz = sparse(S.divzz);
+ area = sparse(S.area);
+ basedx = sparse(S.basedx);
+ basedz = sparse(S.basedz);
+ TensX = S.TensX;
+ TensZ = S.TensZ;
+ voltemp = S.voltemp;
+ fprintf(1, ' done!\n');
+
+ uu = sparse(pCount, 1); %Solution vectors
+ wu = sparse(pCount, 1);
+ Tu = sparse(pCount, 1);
+
+
+AirH = zeros(size(Range,2),1);
+iterN = 0;
+
+
+
+for ourN = 1:size(Range,2)
+  VarN = Range(ourN);
+  
+  iterN = iterN + 1;
+  eval([VarName '=' num2str(VarN) ';']);
+  %newtemp = meantemp;
+  meantemp = T0 + temperr + 1;
+  disp([VarName '=' num2str(VarN)])
+  while(abs(meantemp - T0) > temperr) 
+    T0 = newtemp;
+    bb = 0;%50 + sigma*(0.8*(Tref-30)^4 +  0.2*Tref^4 + 0.8*3*T0^4); %Q due to planck radiation
+    bbt = 0;%-0.8*4*sigma*T0^3; %T-dependance of Q due to planck radiation
+			     %(first order tayolor polynominal)
+
+			     %Boundary conditions. T = TdirichletConditions,
+			     % dT/dn = TneumannConditions +
+                             % T*TneumannTConditions
+     TneumannConditions = [NaN, h*Tref, h*Tref, 0, Uvalue*Tin + bb]/kAir;
+     TneumannTConditions = [NaN,-h, -h, NaN, -Uvalue + bbt]/kAir;
+     TdirichletConditions = [Tref, NaN, NaN, NaN, NaN];
+
+     UdirichletConditions = [0, 0, 0, 0, 0];
+     UneumannConditions = [NaN, NaN, NaN, NaN, NaN];
+     UneumannTConditions = [NaN, NaN, NaN, NaN, NaN];
+
+     WdirichletConditions = [0, 0, 0, 0, 0];
+     WneumannConditions = [NaN, NaN, NaN, NaN, NaN];
+     WneumannTConditions = [NaN, NaN, NaN, NaN, NaN];
+
+     fu = sparse(pCount,1); %Load vectors
+     fw = sparse(pCount, 1);
+     fT = sparse(pCount, 1);
+
+
+     GT = sparse(pCount, 1); %Neumann vectors
+     Gu = sparse(pCount, 1);
+     Gw = sparse(pCount, 1);
+
+     QT = sparse(pCount, pCount); %T dependant neumann matrix
+     LM = sparse(pCount, pCount); %Inhomogenous stiffness matrix
+
+     Tneunan = isnan(TneumannConditions(e(5,:))); %Find active boundary conditions
+     TneuTnan = isnan(TneumannTConditions(e(5,:)));
+     Tdirnan = isnan(TdirichletConditions(e(5,:)));
+
+     Uneunan = isnan(UneumannConditions(e(5,:)));
+     UneuTnan = isnan(UneumannTConditions(e(5,:)));
+     Udirnan = isnan(UdirichletConditions(e(5,:)));
+
+     Wneunan = isnan(WneumannConditions(e(5,:)));
+     WneuTnan = isnan(WneumannTConditions(e(5,:)));
+     Wdirnan = isnan(WdirichletConditions(e(5,:)));
+
+     Tneumann = e([1 2 5], find(~Tneunan)); %Create boundary condition matrices.
+     TneumannT = e([1 2 5], find(~TneuTnan));
+     Tdirichlet = e([1 2 5], find(~Tdirnan));
+
+     Wneumann = e([1 2 5], find(~Wneunan));
+     WneumannT = e([1 2 5], find(~WneuTnan));
+     Wdirichlet = e([1 2 5], find(~Wdirnan));
+
+     Uneumann = e([1 2 5], find(~Uneunan));
+     UneumannT = e([1 2 5], find(~UneuTnan));
+     Udirichlet = e([1 2 5], find(~Udirnan));
+
+     %Create vectors containing the free nodes (non dirichlet nodes)
+     TFreeNodes = setdiff(1:pCount,unique(Tdirichlet));
+     uFreeNodes = setdiff(1:pCount, unique(Udirichlet));
+     wFreeNodes = setdiff(1:pCount, unique(Wdirichlet));
+
+     TFreeCount = size(TFreeNodes,2);
+     uFreeCount = size(uFreeNodes,2);
+     wFreeCount = size(wFreeNodes,2);
+    
+     if(iterN == 1 && T0 == (Tref+3))
+       uGuess = zeros(uFreeCount,1);
+       wGuess = zeros(wFreeCount,1);
+       TGuess = Tref*ones(TFreeCount,1);
+     else
+       uGuess = full(uu(uFreeNodes));
+       wGuess = full(wu(wFreeNodes));
+       TGuess = full(Tu(TFreeNodes));
+     end
+
+     %Create pointers to fields in the results vector.
+     uStart = 1;
+     uEnd = uFreeCount;
+     wStart = uEnd + 1;
+     wEnd = wStart + wFreeCount - 1;
+     TStart = wEnd + 1;
+     TEnd = TStart + TFreeCount - 1;
+
+     uNodes = uStart:uEnd;
+     wNodes = wStart:wEnd;
+     TNodes = TStart:TEnd;
+
+
+     %Assemble load vector
+
+     fprintf(1, 'Assembling load vector...');
+
+     for k = 1:tCount
+       fw(t(1:3,k),1) = fw(t(1:3,k),1) - g*beta*Tref*area(k)/3;
+     end
+
+     fprintf(1, ' done!\n');
+
+     %Assembling load matrix
+
+     fprintf(1, 'Assembling inhomogenous stiffness matrix...');
+
+     for j=1:tCount
+       LM(t(1:3,j), t(1:3,j)) = LM(t(1:3,j), t(1:3,j)) - g*beta*area(j,1)*voltemp;
+     end
+
+     fprintf(1, ' done!\n');
+
+     %Enforce neumann conditions
+     fprintf(1, 'Enforcing neumann conditions...')
+
+
+     for j = 1 :  size(Tneumann,2)
+       GT(Tneumann(1:2,j))=GT(Tneumann(1:2,j)) + ... 
+	   norm(p(:,Tneumann(1,j)) - ... 
+		p(:,Tneumann(2,j)))* ...
+	   alpha*ones(2,1)*TneumannConditions(Tneumann(3,j))/2;
+     end
+
+
+     for j = 1:size(Uneumann,2)
+       Gu(Uneumann(1:2,j))=Gu(Uneumann(1:2,j)) + ... 
+	   norm(p(:,Uneumann(1,j)) - ... 
+		p(:,Uneumann(2,j)))* ...
+	   nu*ones(2,1)*UneumannConditions(Uneumann(3,j))/2;
+     end
+
+     for j = 1:size(Wneumann,2)
+       Gw(Wneumann(1:2,j))=Gw(Wneumann(1:2,j)) + ... 
+	   norm(p(:,Wneumann(1,j)) - ... 
+		p(:,Wneumann(2,j)))* ...
+	   nu*ones(2,1)*WneumannConditions(Wneumann(3,j))/2;
+     end
+     
+     for k = 1:size(TneumannT,2)
+       L = norm(p(:,TneumannT(1,k)) - p(:,TneumannT(2,k)));
+       
+       QT(TneumannT([1 2],k), TneumannT([1 2],k)) = QT(TneumannT([1 ...
+		    2],k),TneumannT([1 2],k)) ...
+	   - alpha*TneumannTConditions(TneumannT(3,k))*L*[2,1;1,2]/6;
+     end
+     
+     fu = fu + Gu;
+     fw = fw + Gw;
+     fT = fT + GT;
+     
+     %Note that we don't have any speed dependance in our Neumann
+     %boundary conditions for the velocity vector.
+     %Also note that we are setting the divergence of the velocity
+     %vector to zero on the border. 
+     
+     fprintf(1, ' done!\n')
+
+
+     
+     fprintf(1, 'Enforcing dirichlet conditions...')
+     uu(:) = 0;
+     wu(:) = 0;
+     Tu(:) = 0;
+
+     Tu(Tdirichlet(1,:)) = TdirichletConditions(Tdirichlet(3,:));
+     Tu(Tdirichlet(2,:)) = TdirichletConditions(Tdirichlet(3,:));
+     uu(Udirichlet(1,:)) = UdirichletConditions(Udirichlet(3,:));
+     uu(Udirichlet(2,:)) = UdirichletConditions(Udirichlet(3,:));
+     wu(Wdirichlet(1,:)) = WdirichletConditions(Wdirichlet(3,:));
+     wu(Wdirichlet(2,:)) = WdirichletConditions(Wdirichlet(3,:));
+
+
+     %Put dirichlet conditions into the solution vectors
+     
+     [int, ca, cb] = intersect(Tdirichlet(1,:), Tdirichlet(2,:));
+     ind = find(Tdirichlet(3,ca) ~= Tdirichlet(3,cb));
+     Tu(int(ind)) = 0.5*(TdirichletConditions(Tdirichlet(3,ca(ind))) + ...
+			 TdirichletConditions(Tdirichlet(3,cb(ind))));
+
+
+     [int, ca, cb] = intersect(Udirichlet(1,:), Udirichlet(2,:));
+     ind = find(Udirichlet(3,ca) ~= Udirichlet(3,cb));
+     uu(int(ind)) = 0.5*(UdirichletConditions(Udirichlet(3,ca(ind))) + ...
+			 UdirichletConditions(Udirichlet(3,cb(ind))));
+
+     [int, ca, cb] = intersect(Wdirichlet(1,:), Wdirichlet(2,:));
+     ind = find(Wdirichlet(3,ca) ~= Wdirichlet(3,cb));
+     wu(int(ind)) = 0.5*(WdirichletConditions(Wdirichlet(3,ca(ind))) + ...
+			 WdirichletConditions(Wdirichlet(3,cb(ind))));
+     
+     %Update load vectors with dirichlet conditions by moving them to
+     %the RHS.
+
+     fu = fu - LHSu(uu, wu, Tu);    
+     fw = fw - LHSw(uu, wu, Tu);
+     fT = fT - LHST(uu, wu, Tu);
+     fprintf(1, ' done!\n');
+
+
+     [resu, resw, resT, ~, ~] = newtonraphson(fu, fw, fT, uGuess, ...
+					      wGuess, TGuess, uFreeNodes, ...
+					      wFreeNodes, ...
+					      TFreeNodes,uNodes,wNodes, TNodes, ITERMAX, epsilon);
+    
+
+     Tu(TFreeNodes) = resT;
+     uu(uFreeNodes) = resu;
+     wu(wFreeNodes) = resw;
+     edges = find(~(e(5,:)-WallID));
+     meantemp = mean(Tu(unique([e(1,edges) e(2,edges)])));
+     disp(['MeanTemp: ' num2str(meantemp) ', T0 = ' num2str(T0)]);
+     newtemp = meantemp;
+  end
+
+  %Calculate the U-value of the air
+  %and put the result in a vector
+
+  %Get the blackbody radiation and solar intensity
+  %NeumannConditions and NeumannTConditions?
+  
+  QConvection = kAir*(TneumannConditions(WallID)+meantemp*TneumannTConditions(WallID));
+  
+  AirH(iterN) = QConvection/(meantemp - Tref);
+  disp(['Found H-value: ' num2str(AirH(iterN))])
+end
+
+figure(1)
+hold off
+plot(Range, AirH)
+xlabel(VarName)
+ylabel('h-value')
+
+
+ret = [Range', AirH];
+
+figure(2)
+tricontourf(p(1:2,:),t(1:3,:),sqrt(uu.^2+wu.^2))
--- code/pdesolver/newtonraphson.m
+++ code/pdesolver/newtonraphson.m
@@ -6,28 +6,182 @@ function [uGuess wGuess TGuess res n]  = newtonraphson(fu, fw, fT, uGuess, ...
 res = epsilon+1;
 n = 0;
 resvec = zeros(max(size(wGuess)+size(uGuess)+size(TGuess)),1);
+resnew = zeros(max(size(wGuess)+size(uGuess)+size(TGuess)),1);
 x = zeros(max(size(resvec)),1);
-
-while ((res > epsilon || n < 2) && n < ITERMAX)
-  resvec(uRange,1) = LHSu(uGuess,wGuess,TGuess,uFreeNodes,wFreeNodes,TFreeNodes) ...
-                   - fu(uFreeNodes);
-  resvec(wRange,1) = LHSw(uGuess,wGuess,TGuess,uFreeNodes, wFreeNodes, ...
+x(uRange) = uGuess;
+x(wRange) = wGuess;
+x(TRange) = TGuess;
+relaxation = 1;
+MaxUpdate = 5;
+wrongwrong = 0;
+resvec(uRange,1) = LHSu(uGuess,wGuess,TGuess,uFreeNodes,wFreeNodes, ...
+			TFreeNodes)- fu(uFreeNodes);
+resvec(wRange,1) = LHSw(uGuess,wGuess,TGuess,uFreeNodes, wFreeNodes, ...
 			  TFreeNodes) - fw(wFreeNodes);
-  resvec(TRange,1) = LHST(uGuess,wGuess,TGuess,uFreeNodes,wFreeNodes,TFreeNodes)...
-                   - fT(TFreeNodes);
+resvec(TRange,1) = LHST(uGuess,wGuess,TGuess,uFreeNodes,wFreeNodes,TFreeNodes)...
+        - fT(TFreeNodes);
+res0 = 0.5*resvec'*resvec;
+rvec = zeros(3,2);
+dxnorm = [];
+minAlpha = 1;
 
+prevAlpha = 1;
+prevG = 0;
+prevRes = 0;
+backtrackCount = 0;
+iterCount = 0;
+
+while ((sqrt(2*res) > epsilon || n < 2) && n < ITERMAX)
+  res = 2*res0;
+  MaxImprove = 13;
+  relaxation = 1;
+  m = 0;
+  %if(wrongwrong == 1)
+  %  disp('OK! Were here')
+  %end
+    
   J = calcJacobian(uGuess, wGuess, TGuess, uFreeNodes, wFreeNodes, ...
-		   TFreeNodes, uRange, wRange, TRange);
-
+                     TFreeNodes, uRange, wRange, TRange);
+ 
+  
+  
+  iterCount = iterCount + 1;
+  
   dx = J\resvec;  
-  x = x - dx;
-  res = norm(resvec);
-  n = n + 1;
-  uGuess = x(uRange);
-  wGuess = x(wRange);
-  TGuess = x(TRange);
+  xnew = x - dx;
+    
+  resnew(uRange,1) = LHSu(xnew(uRange),xnew(wRange),xnew(TRange),uFreeNodes, ...
+			  wFreeNodes,TFreeNodes) - fu(uFreeNodes);
+  resnew(wRange,1) = LHSw(xnew(uRange),xnew(wRange),xnew(TRange),uFreeNodes, wFreeNodes, ...
+			TFreeNodes) - fw(wFreeNodes);
+  resnew(TRange,1) = LHST(xnew(uRange),xnew(wRange),xnew(TRange),...
+			  uFreeNodes,wFreeNodes,TFreeNodes)...
+                          - fT(TFreeNodes);
+  res = 0.5*resnew'*resnew;
 
-  fprintf(1, '%i\t\t%6.4g\t%6.4g\t%6.4g\n', n, norm(resvec(TRange)),...
-          norm(resvec(uRange)), norm(resvec(wRange)))
+  if(res < res0)
+    %Do first jump
+    x = xnew;
+    resvec = resnew;
+    uGuess = x(uRange);
+    wGuess = x(wRange);
+    TGuess = x(TRange);
+    res0 = res;
+    backtrackCount = 0;
+    minAlpha = 1;
+    prevAlpha = 1;
+    prevG = res;
+    iterCount = iterCount + 1;
+    fprintf(1, '%i\t\t%6.4g\t%6.4g\t%6.4g\n', n, norm(resvec(TRange)),...
+              norm(resvec(uRange)), norm(resvec(wRange)))
+
+  else
+
+    %Calculate derivative
+    gprim = -resvec'*resvec;
+    g1 = res;
+    g0 = res0;
+
+    if(backtrackCount == 0)
+      %Minimize the quadratic function
+      
+      backtrackCount = backtrackCount+1;
+      
+      minAlpha = -gprim/(2*(g1-g0-gprim));
+      
+      %if(minAlpha > 0.5*prevAlpha)
+	%minAlpha = 0.5*prevAlpha;
+      %end
+
+      
+    else
+      %Minimize the cubic function
+      backtrackCount = backtrackCount+1;
+      
+      
+      l1 = 1;
+      l2 = prevAlpha;
+      g1 = res;
+      g2 = prevG;
+
+      couf = 1/(l1-l2)*[1/l1^2, -1/l2^2;-l2/l1^2,l1/l2^2]*...
+	     [g1-gprim*l1-res0;g2-gprim*l2-res0];
+
+    
+
+      minAlpha = gprim/(-couf(2) - sqrt(couf(2)^2-3*couf(1)*gprim));
+      %alphavec = [0:0.01:1];
+      %fundi = couf(1)*alphavec.^3+couf(2)*alphavec.^2+gprim* ...
+	%      alphavec+g0;
+      %figure(3)
+      
+      %plot(alphavec, fundi)
+      %hold on
+      %plot(0,g0,'*')
+      %plot(l1, g1, '*')
+      %plot(l2,g2, '*')
+      %plot(minAlpha, couf(1)*minAlpha^3+couf(2)*minAlpha^2+gprim*minAlpha+g0,'o');
+      %hold off
+      %pause
+      if(minAlpha < 0.1*prevAlpha(1,1))
+	%minAlpha = 0.1*prevAlpha(1,1)
+      elseif(minAlpha > 0.5*prevAlpha(1,1))
+	minAlpha = 0.5*prevAlpha(1,1);
+	
+      end
+
+      if(minAlpha < 2^(-15))
+        minAlpha = 1;
+        backtrackCount = 0;
+      end
+
+    end
+
+    if(backtrackCount >= 2)
+      
+     if(mean(deltares)/jumpInitial < 1e-4)
+         minAlpha = 1;
+     end
+    end
+
+    %Perform jump
+    iterCount = iterCount + 1;
+    
+    dx = J\resvec;
+    xnew = x - minAlpha*dx;
+
+    resnew(uRange,1) = LHSu(xnew(uRange),xnew(wRange),xnew(TRange), ...
+			    uFreeNodes, ...            \
+      wFreeNodes,TFreeNodes) - fu(uFreeNodes);
+    resnew(wRange,1) = LHSw(xnew(uRange),xnew(wRange),xnew(TRange), ...
+			    uFreeNodes, wFreeNodes, ...
+                          TFreeNodes) - fw(wFreeNodes);
+    resnew(TRange,1) = LHST(xnew(uRange),xnew(wRange),xnew(TRange),...
+                          uFreeNodes,wFreeNodes,TFreeNodes)...
+                          - fT(TFreeNodes);
+    
+    prevAlpha = minAlpha;
+    x = xnew;
+    res = 0.5*resnew'*resnew;
+    prevG = res;
+    resvec = resnew;
+    uGuess = x(uRange);
+    wGuess = x(wRange);
+    TGuess = x(TRange);
+    
+    if(backtrackCount == 1)
+      jumpInitial = -resnew'*resnew;
+      deltares = [res0-res];
+    else
+      deltares = [deltares; res0-res];
+    end
+    res0 = res;
+      fprintf(1, '%i\t\t%6.4g\t%6.4g\t%6.4g\tAlpha: %6.4g\n', n, norm(resvec(TRange)),...
+              norm(resvec(uRange)), norm(resvec(wRange)),minAlpha)
+    
+  end
+
+  
+  n = n + 1;
 end
 
