23c1e8fb9239bf3149ae4606818c9adbb264875f
Dan Ståby <dstaby@gmail.com>
2012-05-10 08:32:25 +0200

    Fixed the title in some images

--- code/pdesolver/generateWallFigBay.m
+++ code/pdesolver/generateWallFigBay.m
@@ -11,26 +11,26 @@ fig1 = figure(1);
 plot(time, data1(ind,2))
 xlabel('Tid i timmar')
 ylabel('Kyleffekt (W m^{-2})')
-title('Kyleffekt en solig aprildag', 'fontsize', 12)
+title('Kyleffekt utskjutande fasaddelar', 'fontsize', 12)
 xlim([0 24])
 set(fig1, 'Position', [300 700 300 200])
-
+disp('1: Kyleffekt en solig aprildag')
 
 fig2 = figure(2);
 plot(time,data2(ind,2))
-title('Kyleffekt en molning aprildag', 'fontsize', 12)
+title('Kyleffekt utskjutande fasaddelar', 'fontsize', 12)
 xlabel('Tid i timmar')
 ylabel('Kyleffekt (W m^{-2})')
 xlim([0 24])
 
 set(fig2, 'Position', [800 700 300 200])
-
+disp('2: Kyleffekt en molning aprildag')
 
 fig3 = figure(3);
 plot(time,data3(ind,2))
-title('Kyleffekt en molning decemberdag', 'fontsize', 12)
+title('Kyleffekt utskjutande fasaddelar', 'fontsize', 12)
 xlabel('Tid i timmar')
 ylabel('Kyleffekt (W m^{-2})')
 xlim([0 24])
-
+disp('3: Kyleffekt en molnig decemberdag')
 set(fig3, 'Position', [1130 700 300 200])
