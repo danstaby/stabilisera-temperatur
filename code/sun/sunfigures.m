6dd466dd95a05eb71aa9f564320f6d33a86b7ebc
Mats Lindstrom <mats.at.lindstrom@gmail.com>
2012-05-14 10:38:25 +0200

    Edited suns intensity

--- code/sun/sunfigures.m
+++ code/sun/sunfigures.m
@@ -1,4 +1,4 @@
-function ret = sunfigures(month, day, date)
+function ret = sunfigures(month, day, daynumber, date)
 close all
 
 if month>3 month<10
@@ -12,15 +12,20 @@ year = 2011;
 long = 11.979435;
 lat = 57.691522;
 
+
 r = 6.731e6;
 I0 = 1370;
 mu = 4.6e-5;
-
+tau = [0.142,0.144,0.156,0.180,0.196,0.205,0.207,0.201,0.177,0.160,0.1490.142];
+daynumber = 360*(daynumber-1)/365;
+I02 = I0*(1.00011 + 0.034221*cosd(daynumber) + 0.00128*sind(daynumber) + ...
+	  0.000719*cosd(2*daynumber) + 0.000077*sind(daynumber));
 el = [];
 az = [];
 theta = [];
 x = [];
 I = [];
+I2 = [];
 eff = [];
 n = 0;
 xax=[];
@@ -34,11 +39,12 @@ for hr = 0:23
         if el(n) > 0
             x(n) = r*cosd(90+el(n))+sqrt((r*cosd(90+el(n)))^2+((30e3)*r+(15e3)^2));
             I(n) = I0*exp(-mu*x(n));
+            I2(n) = I02*exp(-tau(month)/cosd(90-el(n)));
         else
             x(n) = 0;
             I(n) = 0;
         end
-        eff(n) = effekt(I(n), month, day, UTC);
+        eff(n) = effekt(I2(n), month, day, UTC);
         xax(n)=n;
     end
 end
@@ -52,19 +58,17 @@ xlabel('Tid, UTC+2 [timmar]')
 ylabel('Vinkel [grader]')
 axis([1+sommartid 22+1+sommartid -100 500])
 legend('Relativt horisontella södra riktningen','Höjd över horisonten','Azimuthala relativt öster, medsols positivt')
-%legend(strcat('Relativt horisontella södra riktningen, ', num2str(date)), ...
+%legend(['Relativt horisontella södra riktningen, ' num2str(date)], ...
 %    strcat('Höjd över horisonten, ', num2str(date)), ...
 %    strcat('Azimuthala relativt öster, medsols positivt, ', num2str(date)))
 
 figure(2)
-plot(xax, I)
-hold on
-plot(xax, eff, 'r')
+plot(xax, I2, xax, eff, 'r')
 xlabel('Time, UTC [h]')
-ylabel('Intensity [W/m^2]')
+ylabel('Intensitet [W/m^2]')
 axis([0 24 0 1370])
-legend(strcat('Suns intensity, ', num2str(date)), ...
-    strcat('Flux through window, ', num2str(date)))
+legend(['Solens intensitet, ' num2str(date)], ...
+       ['Intensitet genom glaset, ' num2str(date)])
 
 hold on
 
@@ -72,5 +76,4 @@ figure(3)
 plot(xax, x)
 
 efftotal = (1/60).*trapz(eff);
-I15 = I0*exp(-mu*15e3)
 ret = efftotal;
